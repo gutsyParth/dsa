38fc57741b5bf27063a94bd76379e566ae5b629c
Parth Yadav <gutsyparthyadav@gmail.com>
2026-07-06 18:52:33 +0530

    Daily progress - 2026-07-05

---
+++ 1929.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        int N = nums.size();
+        vector<int> ans(2 * N);
+
+        for (int i = 0; i < N; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + N] = nums[i];
+        }
+
+        return ans;
+    }
+};
